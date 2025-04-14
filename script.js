7704e59ae724a5ff3b4e8f9e5b1c09f6b8110825
hobertnice <eliaspereira0827@gmail.com>
2025-04-14 18:39:28 -0300

    Adicionando barra de navegação fixa e estilos

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+window.onload = function() {
+  alert("Bem-vindo ao meu site!");
+}
+window.onload = function() {
+  document.getElementById("loader").style.display = "none";
+}
